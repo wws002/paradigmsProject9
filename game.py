b2193ace111bdf415ce8ba3b5e63e21d31e547aa
William Smith <wws002@email.uark.edu>
2018-12-06 20:29:21 -0600

    cleaned up

--- game.py
+++ game.py
@@ -11,26 +11,17 @@ class Mario():
 		self.x = 0;
 		self.y = 0;
 
+	def update(self):
+		self.y = 0;
+
 
 
 class Model():
 	def __init__(self):
-		self.dest_x = 0
-		self.dest_y = 0
+		self.mario = Mario();
 
 	def update(self):
-		if self.rect.left < self.dest_x:
-			self.rect.left += 1
-		if self.rect.left > self.dest_x:
-			self.rect.left -= 1
-		if self.rect.top < self.dest_y:
-			self.rect.top += 1
-		if self.rect.top > self.dest_y:
-			self.rect.top -= 1
-
-	def set_dest(self, pos):
-		self.dest_x = pos[0]
-		self.dest_y = pos[1]
+		self.mario.update();
 
 
 
@@ -43,8 +34,9 @@ class View():
 		self.model.rect = self.mario1.get_rect()
 
 	def update(self):
-		self.screen.fill([0,200,100])
-		self.screen.blit(self.mario1, self.model.rect)
+		self.screen.fill([0,200,200])#draw the sky(sunny)
+		pygame.draw.rect(self.screen, (0, 128, 0), (0, 450, 800, 600))#draw the ground
+		self.screen.blit(self.mario1, self.model.rect)#draw mario
 		pygame.display.flip()
 
 
@@ -61,17 +53,11 @@ class Controller():
 			elif event.type == KEYDOWN:
 				if event.key == K_ESCAPE:
 					self.keep_going = False
-			elif event.type == pygame.MOUSEBUTTONUP:
-				self.model.set_dest(pygame.mouse.get_pos())
 		keys = pygame.key.get_pressed()
-		if keys[K_LEFT]:
-			self.model.dest_x -= 1
-		if keys[K_RIGHT]:
-			self.model.dest_x += 1
-		if keys[K_UP]:
-			self.model.dest_y -= 1
-		if keys[K_DOWN]:
-			self.model.dest_y += 1
+		#if keys[K_LEFT]:
+		#	self.model.dest_x -= 1
+		#if keys[K_RIGHT]:
+		#	self.model.dest_x += 1
 
 
 
