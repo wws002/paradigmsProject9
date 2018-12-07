49ec2b6155d228478e8e93e02e5d8ae781fc3489
William Smith <wws002@email.uark.edu>
2018-12-06 23:43:48 -0600

    mario is now running and jumping

--- game.py
+++ game.py
@@ -61,10 +61,12 @@ class Controller():
 				if event.key == K_ESCAPE:
 					self.keep_going = False
 		keys = pygame.key.get_pressed()
-		#if keys[K_LEFT]:
-		#	self.model.dest_x -= 1
-		#if keys[K_RIGHT]:
-		#	self.model.dest_x += 1
+		if keys[K_LEFT]:
+			self.model.mario.x -= 10
+		if keys[K_RIGHT]:
+			self.model.mario.x += 10
+		if keys[K_SPACE] and self.model.mario.y == 355:
+			self.model.mario.vvel = -30
 
 
 
